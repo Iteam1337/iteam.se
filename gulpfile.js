db74bae1850187089de2cf25cfb883b96cbef9e1
Rickard Laurin <rickard.laurin@gmail.com>
2014-07-08 15:21:57 +0200

    switch to handlebars and lots of stuff

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,15 @@ var gulp       = require('gulp')
 ,   jade       = require('gulp-jade')
 ,   rename     = require('gulp-rename');
 
+var assemble = require('gulp-assemble');
+var htmlmin = require('gulp-htmlmin');
+
 var config = {
   styles: 'src/less/**/',
   stylesOut: 'out/css/',
   allStyle: '*.less',
   mainStyle: 'main.less',
-  documents: 'src/document/**/*.jade'
+  documents: 'src/pages/**/*.hbs'
 };
 
 gulp.task('connect', function() {
@@ -23,6 +26,10 @@ gulp.task('connect', function() {
 gulp.task('copy', function () {
   gulp.src('src/content/**/*')
     .pipe(gulp.dest('out/content'));
+
+  gulp.src('src/scripts/**/*')
+    .pipe(gulp.dest('out/scripts'))
+    .pipe(connect.reload());
 });
 
 gulp.task('less', function () {
@@ -36,18 +43,30 @@ gulp.task('less', function () {
     .pipe(connect.reload());
 });
 
-gulp.task('templates', function() {
-  gulp.src(config.documents)
+var options = {
+  data: 'src/data/*.json',
+  partials: 'src/partials/*.hbs',
+  layoutdir: 'src/layouts/',
+  helpers: ['src/scripts/handlebar-helpers.js']
+};
+
+gulp.task('assemble', function () {
+  gulp.src([config.documents])
     .pipe(plumber())
-    .pipe(jade())
+    .pipe(assemble(options))
+    .pipe(htmlmin({
+      collapseWhitespace:true
+    }))
+    .pipe(rename('index.html'))
     .pipe(gulp.dest('out/'))
     .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-  gulp.watch([config.documents, 'src/jade_includes/**/*.jade'], ['templates']);
+  gulp.watch([config.documents, 'src/partials/**/*.hbs', 'src/data/**/*.json'], ['assemble']);
   gulp.watch([config.styles + config.allStyle], ['less']);
   gulp.watch('src/content/**/*', ['copy']);
+  gulp.watch('src/scripts/**/*', ['copy']);
 });
 
-gulp.task('default', ['copy', 'less', 'templates', 'connect', 'watch']);
+gulp.task('default', ['copy', 'less', 'assemble', 'connect', 'watch']);
