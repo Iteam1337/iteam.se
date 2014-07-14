e533c3b8ed62896dd1100e27ff2aade48f800658
Rickard Laurin <rickard.laurin@gmail.com>
2014-07-14 17:14:24 +0200

    fixes #27 - sort order in pages helper

--- src/helpers/pages.js
+++ src/helpers/pages.js
@@ -9,7 +9,7 @@ module.exports.pages = function (route, start, options) {
     return fs.statSync(dir + file).isDirectory();
   });
 
-  var cases = dirs.map(function (folder) {
+  var pages = dirs.map(function (folder) {
     var frontmatter = front.loadFront(dir + folder + '/index.hbs');
     var title = frontmatter.subtitle || frontmatter.name;
 
@@ -17,8 +17,15 @@ module.exports.pages = function (route, start, options) {
                     '<a href="' + lead + folder + '/">' + title + '</a>'+
                   '</li>';
 
-    return listUrl;
+    return {
+      element: listUrl,
+      order: frontmatter.order || undefined
+    };
+  }).sort(function (a,b) {
+    return a.order - b.order;
+  }).map(function (page) {
+    return page.element;
   });
 
-  return cases.join().replace(/\,/g,'');
+  return pages.join().replace(/\,/g,'');
 };
