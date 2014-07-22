af5ca79a66839d87c509616cb84d581ed53b0617
Radu Achim <radu.achim@iteam.se>
2014-07-22 14:22:39 +0200

    updated tests for pages helper, pages returns data and the templates take care of rendering #32

--- src/helpers/pages.js
+++ src/helpers/pages.js
@@ -3,61 +3,38 @@ var front = require('yaml-front-matter');
 var image = require('./gravatar');
 var read  = require('./read');
 
-// module.exports.pages = function (route, start, type, size, options) {
 module.exports.pages = function (data, options) {
   var pages;
   var dir = data.route || './src/pages/case/';
-  var lead = data.start || '';
   var dirs = read.directory(dir);
+  var lead = data.start || '';
   var type = data.type;
   var size = data.size;
 
   pages = dirs.map(function (folder) {
     var frontmatter;
     var title;
-    var listUrl;
     var logo;
-    var url;
     var lastName;
-    var categories;
 
     frontmatter = front.loadFront(dir + folder + '/index.hbs');
-    categories = frontmatter.categories || [];
-    title       = frontmatter.subtitle || frontmatter.name;
-    url         = '<a href="' + lead + folder + '/">' + title + '</a>';
-    logo        = frontmatter.logo ? '<img src="' + frontmatter.logo + '">' : '';
+    title = frontmatter.subtitle || frontmatter.name;
+    logo = frontmatter.logo ? 
+      frontmatter.logo : 
+      '';
 
-    if (type === 'coworker') {
+    if(type === 'coworker') {
       var imgSize = size || false;
-      var img = image.gravatar(frontmatter.email, imgSize);
+      logo = image.gravatar(frontmatter.email, imgSize);
       lastName = title.substr(title.lastIndexOf(' ') + 1);
-
-      listUrl = '<li>'+
-                  '<a href="' + lead + folder + '">'+
-                    '<img src="' + img + '">'+
-                  '</a>'+
-                  '<div>'+
-                    url +
-                    '<p>'+ frontmatter.job + '</p>'+
-                  '</div>'+
-                '</li>';
-    } else if (type === 'services') {
-      listUrl = '<li>'+
-            '<i class="' + frontmatter.icon + '"></i>'+
-            '<div>'+
-              url+
-              '<p>'+ frontmatter.description + '</p>'+
-            '</div>'+
-          '</li>';
-    } else {
-      listUrl = '<li class="' + categories.join().replace(/\,/g, ' ') +'">'+ logo + url + '</li>';
     }
+   
     return {
       element: {
-        categories: categories.join().replace(/\,/g, ' '),
-        logo: logo,
-        url: url,
-        frontmatter: frontmatter
+        frontmatter: frontmatter,
+        url: lead + folder,
+        title: title,
+        logo: logo
       },
       order: frontmatter.order || lastName
     };
@@ -72,7 +49,5 @@ module.exports.pages = function (data, options) {
   }).map(function (page) {
     return page.element;
   });
-  console.log(pages);
   return options.fn({ data: pages });
-  // return pages.join().replace(/\,/g,'');
 };
--- src/test/spec/pages-tests.js
+++ src/test/spec/pages-tests.js
@@ -1,19 +1,38 @@
-var chai       = require('chai')
-,   expect     = chai.expect
-,   sinon      = require('sinon')
-,   proxyquire = require('proxyquire');
+'use strict';
+var chai = require('chai');
+var expect = chai.expect;
+var sinon = require('sinon');
+var proxyquire = require('proxyquire');
 
 describe('#pages', function () {
   var image;
   var helper;
+  var options;
+  var front;
+  var read;
 
   beforeEach(function () {
+    front = {
+      loadFront: sinon.stub().returns({
+        name: 'foo',
+        email: 'rickard.laurin@iteam.se'
+      })
+    };
+    read = {
+      directory: sinon.stub().returns(['foo'])
+    };
     image = {
       gravatar: sinon.stub().returns('http://www.gravatar.com')
     };
 
+    options = {
+      fn: sinon.spy()
+    };
+
     helper = proxyquire(process.cwd() + '/src/helpers/pages', {
-      './gravatar': image
+      './gravatar': image,
+      './read': read,
+      'yaml-front-matter': front
     });
   });
 
@@ -21,35 +40,81 @@ describe('#pages', function () {
     expect(helper.pages).to.be.a('function');
   });
 
-  it('should add a lead character to the href', function () {
-    expect(helper.pages('./src/pages/', '/')).to.be.a('string');
-    expect(helper.pages('./src/pages/', '/')).to.match(/^<li class=""><a\shref="\/.+?<\/li>$/);
+  it('should add a lead character to the url', function () {
+    helper.pages({
+      route: './src/pages/', 
+      start: '/'
+    }, options);
+    expect(options.fn).calledOnce;
+    expect(options.fn).calledWith({
+      data: [{
+        frontmatter: {
+          name: 'foo',
+          email: 'rickard.laurin@iteam.se'
+        },
+        logo: '',
+        title: 'foo',
+        url: '/foo'
+      }]
+    });
   });
 
-  it('should return a list when given a route', function () {
-    expect(helper.pages('./src/pages/')).to.be.a('string');
-    expect(helper.pages('./src/pages/')).to.match(/^<li class=""><a\shref=".+?<\/li>$/);
-  });
-
-  it('should return list of cases when not given a route', function () {
-    expect(helper.pages()).to.be.a('string');
-    expect(helper.pages()).to.match(/^<li class=""><a.+?<\/li>$/);
+  it('should call the handler when given a route', function () {
+    helper.pages({ route: './src/pages/' }, options);
+    expect(options.fn).calledOnce;
+    expect(options.fn).calledWith({
+      data: [{
+        frontmatter: {
+          name: 'foo',
+          email: 'rickard.laurin@iteam.se'
+        },
+        logo: '',
+        title: 'foo',
+        url: 'foo'
+      }]
+    });
   });
 
   it('should get the gravatars if it is a coworker', function () {
-    helper.pages('./src/pages/medarbetare/','','coworker');
+    helper.pages({
+      route: './src/pages/medarbetare/', 
+      type: 'coworker'
+    }, options);
     expect(image.gravatar).called.and.calledWith('rickard.laurin@iteam.se', false);
   });
 
-  it('should return a different template if type is coworker', function () {
-    expect(helper.pages('./src/pages/medarbetare/','','coworker')).to.match(/^<li><a.+?<\/li>$/);
-  });
-
-  it('should return a different template if type is services', function () {
-    expect(helper.pages('./src/pages/medarbetare/','','services')).to.match(/^<li><i.+?<\/li>$/);
-  });
-
   it('should sort by last name', function () {
-    expect(helper.pages('./src/pages/medarbetare/','','coworker')).to.match(/^<li><a href="radu">.+?<\/li>$/);
+    read.directory.returns(['foo', 'bar']);
+    front.loadFront.withArgs('./src/pages/medarbetare/foo/index.hbs').returns({
+      name: 'foo',
+      email: 'rickard.laurin@iteam.se'
+    });
+    front.loadFront.withArgs('./src/pages/medarbetare/bar/index.hbs').returns({
+      name: 'bar',
+      email: 'radu.achim@iteam.se'
+    });
+    helper.pages({
+      route: './src/pages/medarbetare/',
+      type: 'coworker'
+    }, options);
+    expect(options.fn).calledWith({
+      data: [{
+        frontmatter: {
+          name: 'bar',
+          email: 'radu.achim@iteam.se'
+        },
+        logo: 'http://www.gravatar.com',
+        title: 'bar',
+        url: 'bar'
+      }, {
+        frontmatter: {
+          name: 'foo',
+          email: 'rickard.laurin@iteam.se'
+        },
+        logo: 'http://www.gravatar.com',
+        title: 'foo',
+        url: 'foo'
+      }]
+    });
   });
 });
