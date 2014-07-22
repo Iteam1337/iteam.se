e27616589208db040d78fcfac7f0514a8b0a9a21
Dennis Pettersson <mail@dennispettersson.se>
2014-07-22 23:31:31 +0200

    Added a js/css animation "framework"
    All that is needed for this to work is that one adds two css classes:
      animate AND a animation-class.
    
    data-delay and data-duration is optional.
    All the animations are done with css, the javascript just toggles them
    to be triggered when they are in the viewport.
    
    All transitions are borrowed from [http://daneden.me/animate](http://daneden.me/animate)

---
+++ src/scripts/animate.js
@@ -0,0 +1,39 @@
+(function () {
+  'use strict';
+  function animate(element, visible) {
+    if (visible) {
+      element.classList.add('animate');
+    } else {
+      element.classList.remove('animate');
+    }
+  }
+  function update() {
+    for (var i = 0, max = content.length, element; i < max; i++) {
+      element = content[i];
+      animate(element, isElementInViewport(element));
+    }
+  }
+  function isElementInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (rect.top <= window.innerHeight);
+  }
+  function setOptions(element) {
+    var prefixes = ['-webkit-','-moz-','-ms-','-o-','',''];
+    var delay = element.getAttribute('data-delay') || '0';
+    delay = prefixes.join('animation-delay:' + delay + ';');
+
+    var duration = element.getAttribute('data-duration') || '5s';
+    duration = prefixes.join('animation-duration:' + duration + ';');
+
+    var css = delay + duration + element.style.cssText;
+    element.style.cssText = css;
+  }
+  var content = document.getElementsByClassName('animated');
+  if (content.length) {
+    for (var i = 0, max = content.length; i < max; i++) {
+      setOptions(content[i]);
+    }
+    update();
+    window.addEventListener('scroll', update, false);
+  }
+})();
