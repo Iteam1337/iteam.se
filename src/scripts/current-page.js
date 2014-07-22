b70975e83e5270389ca0851aaf5ec1415a4bddbd
Rickard Laurin <rickard.laurin@gmail.com>
2014-07-22 17:21:17 +0200

    fixes #41 - Shows active page in navbar

---
+++ src/scripts/current-page.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var path = window.location.pathname.split('/')[1];
+var navbar = document.querySelectorAll('#navbar ul li');
+
+for (var i = 0; i < navbar.length; i++) {
+  var child = navbar[i].children[0];
+
+  if ( child.getAttribute('href').substr(1) === path ) {
+    child.classList.add('active');
+  }
+}
