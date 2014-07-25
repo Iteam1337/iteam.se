7accf8c4ed55ea8fc2fa7e7986dceb51406d45cc
Radu Achim <radu.achim@iteam.se>
2014-07-25 09:40:35 +0200

    team helper creates the data for the template to use fix #46

--- src/test/spec/team-tests.js
+++ src/test/spec/team-tests.js
@@ -1,39 +1,54 @@
-var chai       = require('chai')
-,   expect     = chai.expect
-,   sinon      = require('sinon')
-,   proxyquire = require('proxyquire');
+'use strict';
+var chai       = require('chai');
+var expect     = chai.expect;
+var sinon      = require('sinon');
+var proxyquire = require('proxyquire');
 
 chai.use(require('sinon-chai'));
 
 describe('#team', function () {
   var image;
   var helper;
+  var options;
+  var front;
 
   beforeEach(function () {
+    options = {
+      fn: sinon.spy()
+    };
+
     image = {
       gravatar: sinon.stub().returns('http://www.gravatar.com')
     };
 
+    front = {
+      loadFront: sinon.stub()
+    };
+
     helper = proxyquire(process.cwd() + '/src/helpers/team', {
-      './gravatar': image
+      './gravatar': image,
+      'yaml-front-matter': front
     });
   });
-
-  it('should be a function', function () {
-    expect(helper.team).to.be.a('function');
-  });
-
-  it('should return a list template', function () {
-    expect(helper.team(['rickard'])).to.match(/<li><a.+?<\/li>/);
-  });
-
-  it('should not matter if name is capitalized', function () {
-    expect(helper.team(['Rickard'])).to.match(/<li><a.+?<\/li>/);
-  });
-
   it('should call for a gravatar', function () {
-    helper.team(['rickard']);
-
+    front.loadFront.returns({ email: 'rickard.laurin@iteam.se' });
+    helper.team({ team: ['rickard'] }, options);
     expect(image.gravatar).calledOnce.and.calledWith('rickard.laurin@iteam.se', false);
+  });
+  it('should call options.fn with the correct parameter', function () {
+    front.loadFront.withArgs('./src/pages/medarbetare/rickard/index.hbs').returns({ email: 'rickard.laurin@iteam.se' });
+    front.loadFront.withArgs('./src/pages/medarbetare/dennis/index.hbs').returns({ email: 'dennis.pettersson@iteam.se' });
+    helper.team({ team: ['rickard', 'dennis'], 'gravatar-sizes': 200 }, options);
+    expect(options.fn).calledOnce.and.calledWith({
+      data: [{
+        frontmatter: { email: 'rickard.laurin@iteam.se' },
+        img: 'http://www.gravatar.com',
+        url: '/medarbetare/rickard'
+      }, {
+        frontmatter: { email: 'dennis.pettersson@iteam.se' },
+        img: 'http://www.gravatar.com',
+        url: '/medarbetare/dennis'
+      }]
+    });
   });
 });
