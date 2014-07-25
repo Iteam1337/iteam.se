988882fc430cbae180474e92d6622c946101bd1b
Radu Achim <radu.achim@iteam.se>
2014-07-25 13:40:55 +0200

    calendar partial, helper to take the yaml and filter older entries fix #8

---
+++ src/helpers/calendar.js
@@ -0,0 +1,20 @@
+'use strict';
+var front = require('yaml-front-matter');
+var moment = require('moment');
+
+module.exports.calendar = function (data, options) {
+  var frontmatter = front.loadFront('./src/pages/index.hbs');
+  var calendar = frontmatter.partials.filter(function (partial) { 
+    return partial.type === 'calendar'; 
+  })[0];
+
+  if(!calendar) {
+    return '';
+  }
+
+  var filtered = calendar.content.filter(function (entry) {
+    return moment(entry.time).isAfter(moment().subtract(30, 'day'));
+  });
+
+  return options.fn(filtered);
+};
