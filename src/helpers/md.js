a394bbdccf25501cbd3ab2edd2a208dda0f14f97
Rickard <rickard.laurin@gmail.com>
2015-09-01 21:05:29 +0200

    Fixed md helper

--- src/helpers/md.js
+++ src/helpers/md.js
@@ -5,10 +5,6 @@ var fs     = require('fs');
 var path   = require('path');
 
 module.exports.md = function (filePath, options) {
-  if (filePath.indexOf('/') === -1) {
-    return marked(filePath);
-  }
-  
   var dirName = path.dirname(options.data.orig.files[0].src);
 
   if(null === filePath.match(/\//g)) {
