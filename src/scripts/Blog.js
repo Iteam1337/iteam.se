97f5714eb64f255ce0bce97e58c7e62e19e9b42a
Christian Landgren <cl@iteam.se>
2015-09-07 17:18:46 +0200

    insta-team now loads over https

--- src/scripts/Blog.js
+++ src/scripts/Blog.js
@@ -1,54 +1,54 @@
-'use strict';
+'use strict'
 
-function Blog(count) {
-  Social.apply(this, ['blog', count]);
-  this.baseURL = "http://feed.insta-team.se/rss?url={url}&count={count}";
+function Blog (count) {
+  Social.apply(this, ['blog', count])
+  this.baseURL = '//feed.insta-team.se/rss?url={url}&count={count}'
 }
 
-Blog.prototype = Object.create(Social.prototype);
-Blog.prototype.constructor = Social;
+Blog.prototype = Object.create(Social.prototype)
+Blog.prototype.constructor = Social
 
 Blog.prototype._init = function (container, url) {
-  this.url = url;
-  return this.init(container, url.replace(/\W/g, ''));
-};
+  this.url = url
+  return this.init(container, url.replace(/\W/g, ''))
+}
 
 Blog.prototype.URL = function () {
   return this.baseURL
     .replace('{url}', escape(this.url))
-    .replace('{count}', this.count);
-};
+    .replace('{count}', this.count)
+}
 
 Blog.prototype.prerender = function (array) {
-  var newElement = document.createElement('ul');
-  var node, a, p, tempNode, h3;
+  var newElement = document.createElement('ul')
+  var node, a, p, tempNode, h3
   for (var i = 0, max = array.length, data; i < max; i++) {
-    data = array[i];
+    data = array[i]
 
-    h3 = document.createElement('h3');
-    a = document.createElement('a');
-    a.setAttribute('href', data.link);
-    a.setAttribute('target', '_blank');
-    a.innerHTML = data.title;
+    h3 = document.createElement('h3')
+    a = document.createElement('a')
+    a.setAttribute('href', data.link)
+    a.setAttribute('target', '_blank')
+    a.innerHTML = data.title
 
-    tempNode = document.createElement('pre');
-    tempNode.innerHTML = data.content;
+    tempNode = document.createElement('pre')
+    tempNode.innerHTML = data.content
 
-    p = document.createElement('p');
-    p.innerHTML = (tempNode.textContent || tempNode.innerText || '').slice(0, 120) + ' …';
+    p = document.createElement('p')
+    p.innerHTML = (tempNode.textContent || tempNode.innerText || '').slice(0, 120) + ' …'
 
-    node = document.createElement('li');
+    node = document.createElement('li')
 
-    h3.appendChild(a);
-    node.appendChild(h3);
-    node.appendChild(p);
+    h3.appendChild(a)
+    node.appendChild(h3)
+    node.appendChild(p)
 
-    newElement.appendChild(node);
+    newElement.appendChild(node)
   }
-  return newElement;
-};
+  return newElement
+}
 
 Blog.prototype.handleResponse = function (response) {
-  response = JSON.parse(response).data;
-  return [response, response[0].pubDate];
-};
+  response = JSON.parse(response).data
+  return [response, response[0].pubDate]
+}
--- src/scripts/SocialHub.js
+++ src/scripts/SocialHub.js
@@ -1,80 +1,80 @@
-'use strict';
+'use strict'
 
-function SocialHub(type, count, resolution) {
-  Social.apply(this, [type, count]);
+function SocialHub (type, count, resolution) {
+  Social.apply(this, [type, count])
 
-  this.url = 'http://insta-team.se/';
+  this.url = '//insta-team.se/'
   if (this.type === 'twitter') {
-    this.url += 'twitter/';
+    this.url += 'twitter/'
   }
-  this.url += 'user/';
+  this.url += 'user/'
 
-  this.resolution = (resolution || '').toLowerCase();
+  this.resolution = (resolution || '').toLowerCase()
   if (this.resolution !== 'low' && this.resolution !== 'standard') {
-    this.resolution = 'standard';
+    this.resolution = 'standard'
   }
-  this.resolution += '_resolution';
+  this.resolution += '_resolution'
 }
 
-SocialHub.prototype = Object.create(Social.prototype);
-SocialHub.prototype.constructor = Social;
+SocialHub.prototype = Object.create(Social.prototype)
+SocialHub.prototype.constructor = Social
 
 SocialHub.prototype.parseLinks = function (text) {
-  var regex = /((http(s)?:\/\/)|\b|^)[a-zA-Z0-9\-_\.]+\.[a-zA-Z]{2,3}(:[a-zA-Z0-9]*)?([a-zA-Z0-9åäö\!\&\-\.\_\?\,\'\/\\\+\;\%\$\#\=\~\:\(\)\@])*/gi;
+  var regex = /((http(s)?:\/\/)|\b|^)[a-zA-Z0-9\-_\.]+\.[a-zA-Z]{2,3}(:[a-zA-Z0-9]*)?([a-zA-Z0-9åäö\!\&\-\.\_\?\,\'\/\\\+\;\%\$\#\=\~\:\(\)\@])*/gi
   return text.replace(regex, function (str) {
-    return '<a target="_blank" href="' + ( ( !arguments[ 2 ] ) ? 'http://' + str : str ) + '">' + str + '</a>';
-  });
-};
+    return '<a target="_blank" href="' + ( ( !arguments[ 2 ]) ? '//' + str : str ) + '">' + str + '</a>'
+  })
+}
 
 SocialHub.prototype.URL = function () {
-  return this.url + this.handle + '?count=' + this.count;
-};
+  return this.url + this.handle + '?count=' + this.count
+}
 
 SocialHub.prototype.prerender = function (array) {
   function getType (str) {
-    var i = str.lastIndexOf('.');
-    return 'video/' + str.substr(i + 1);
+    var i = str.lastIndexOf('.')
+    return 'video/' + str.substr(i + 1)
   }
-  var newElement = document.createElement('div');
-  var type = '';
-  var node = null, source = null, video = null, image = null;
+  var newElement = document.createElement('div')
+  var type = ''
+  var node = null, source = null, video = null, image = null
   for (var i = 0, max = array.length, data; i < max; i++) {
-    data = array[i];
+    data = array[i]
     if (this.type === 'twitter') {
-      node = document.createElement('div');
-      node.innerHTML = this.parseLinks(data.text);
+      node = document.createElement('div')
+      node.innerHTML = this.parseLinks(data.text)
     } else if (data.type === 'image') {
-      node = document.createElement('img');
-      image = data.image[this.resolution];
-      node.setAttribute('src', image.url);
-      node.setAttribute('width', image.width);
-      node.setAttribute('height', image.height);
-      node.setAttribute('href', data.link);
-      node.setAttribute('target', '_blank');
+      node = document.createElement('img')
+      image = data.image[this.resolution]
+      node.setAttribute('src', image.url)
+      node.setAttribute('width', image.width)
+      node.setAttribute('height', image.height)
+      node.setAttribute('href', data.link)
+      node.setAttribute('target', '_blank')
     } else if (data.type === 'video') {
-      node = document.createElement('video');
-      source = document.createElement('source');
-      video = data.video[this.resolution];
-      node.setAttribute('poster', data.image[this.resolution].url);
-      node.setAttribute('width', video.width);
-      node.setAttribute('height', video.height);
-      node.setAttribute('controls', 'controls');
-      source.setAttribute('src', video.url);
-      source.setAttribute('type', getType(video.url));
-      node.appendChild(source);
+      node = document.createElement('video')
+      source = document.createElement('source')
+      video = data.video[this.resolution]
+      node.setAttribute('poster', data.image[this.resolution].url)
+      node.setAttribute('width', video.width)
+      node.setAttribute('height', video.height)
+      node.setAttribute('controls', 'controls')
+      source.setAttribute('src', video.url)
+      source.setAttribute('type', getType(video.url))
+      node.appendChild(source)
     } else {
-      node = document.createElement('span');
+      node = document.createElement('span')
     }
-    newElement.appendChild(node);
+    newElement.appendChild(node)
   }
-  return newElement;
-};
+  return newElement
+}
 
 SocialHub.prototype.handleResponse = function (response) {
-  console.log(response);
-  response = JSON.parse(response);
+  console.log(response)
+  response = JSON.parse(response)
   if (response.error !== null) {
-    console.error(response.error);
+    console.error(response.error)
   }
-  return [response.data, response.data[0].created_at];
-};
+  return [response.data, response.data[0].created_at]
+}
