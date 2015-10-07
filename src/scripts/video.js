b8102ec512f343b8ad013cf08121c3c6a9334a51
Dennis Pettersson <mail@dennispettersson.se>
2015-10-07 17:00:10 +0200

    Corrected the js-error and updated playback speed
    The speed is set to .8 for playbackRate
    It is customisable from the templates

--- src/scripts/video.js
+++ src/scripts/video.js
@@ -1,5 +1,13 @@
 (function () {
   'use strict';
 
-  Array.prototype.slice.call();
+  var videoElements = document.querySelectorAll('video[playbackrate]');
+  if (!videoElements.length) {
+    return;
+  }
+
+  Array.prototype.slice.call(videoElements).forEach(function (element) {
+    var playbackRate = element.getAttribute('playbackrate');
+    element.playbackRate = playbackRate;
+  });
 })();
