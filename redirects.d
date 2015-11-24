6530e152f4c13df32852459b89db01f375364299
Dennis Pettersson <mail@dennispettersson.se>
2015-11-24 12:30:47 +0100

    Added a template for a 404 page

--- redirects.d
+++ redirects.d
@@ -2,3 +2,30 @@ if ($request_uri ~ medarbetare) {
   rewrite /medarbetare/(.*) /coworkers/$1 permanent;
   break;
 }
+
+if ($request_uri ~ karriar) {
+  rewrite /karriar/(.*) /career/$1 permanent;
+  break;
+}
+
+if ($request_uri ~ fjarrhjalp) {
+  rewrite /fjarrhjalp/(.*) /remoteassistance/$1 redirect;
+  break;
+}
+
+if ($request_uri ~ operations) {
+  rewrite /operations/(.*) /remoteassistance/$1 redirect;
+  break;
+}
+
+if ($request_uri ~ "om-oss") {
+  rewrite /om\-oss/(.*) /aboutus/$1 permanent;
+  break;
+}
+
+if ($request_uri ~ "vara-tjanster") {
+  rewrite /vara\-tjanster/(.*) /$1 redirect;
+  break;
+}
+
+error_page 404 /404/index.html;
