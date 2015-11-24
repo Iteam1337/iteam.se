4dc7f26e7e00b24b506e78fa4413419f120e66e9
Dennis Pettersson <mail@dennispettersson.se>
2015-11-24 10:49:53 +0100

    Added more redirects

--- redirects.d
+++ redirects.d
@@ -2,3 +2,28 @@ if ($request_uri ~ medarbetare) {
   rewrite /medarbetare/(.*) /coworkers/$1 permanent;
   break;
 }
+
+if ($request_uri ~ karriar) {
+  rewrite /karriar/(.*) /career/$1 permanent;
+  break;
+}
+
+if ($request_uri ~ fjarrhjalp) {
+  rewrite /fjarrhjalp/(.*) /remoteassistance/$1 307;
+  break;
+}
+
+if ($request_uri ~ operations) {
+  rewrite /operations/(.*) /remoteassistance/$1 307;
+  break;
+}
+
+if ($request_uri ~ "om-oss") {
+  rewrite /om-oss/(.*) /aboutus/$1 permanent;
+  break;
+}
+
+if ($request_uri ~ "vara-tjanster") {
+  rewrite /vara-tjanster/(.*) /$1 307;
+  break;
+}
