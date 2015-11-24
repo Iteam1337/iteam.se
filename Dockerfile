f133f435d35021d396e985699c6c4fa45a82e5d0
Dennis Pettersson <mail@dennispettersson.se>
2015-11-24 15:05:49 +0100

    Handling redirects and 404s

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM tutum.co/iteamdev/node-gulp:0.12
 
 ADD package.json /app/
-RUN npm install
 COPY redirects.d /etc/nginx/
-
 ADD src /app/src
 ADD gulpfile.js /app/
 ADD bower.json /app/
+
+RUN npm install
 RUN ./node_modules/.bin/bower install --config.interactive=false --allow-root
 RUN ./node_modules/.bin/gulp build
--- redirects.d
+++ redirects.d
@@ -1,31 +1,36 @@
-if ($request_uri ~ medarbetare) {
-  rewrite /medarbetare/(.*) /coworkers/$1 permanent;
-  break;
-}
-
-if ($request_uri ~ karriar) {
-  rewrite /karriar/(.*) /career/$1 permanent;
-  break;
-}
-
-if ($request_uri ~ fjarrhjalp) {
-  rewrite /fjarrhjalp/(.*) /remoteassistance/$1 redirect;
-  break;
-}
-
-if ($request_uri ~ operations) {
-  rewrite /operations/(.*) /remoteassistance/$1 redirect;
-  break;
-}
-
-if ($request_uri ~ "om-oss") {
-  rewrite /om\-oss/(.*) /aboutus/$1 permanent;
-  break;
-}
-
-if ($request_uri ~ "vara-tjanster") {
-  rewrite /vara\-tjanster/(.*) /$1 redirect;
-  break;
-}
-
 error_page 404 /404/index.html;
+
+location ^~ /services/ { return 404; }
+location ^~ /karriar/supporttekniker/ { return 404; }
+location ^~ /om-oss/kompetens/att-bestalla-agilt/ { return 404; }
+location ^~ /case/amabv/ { return 404; }
+location ^~ /case/wakers/ { return 404; }
+location ^~ /case/landahl/ { return 404; }
+location ^~ /case/beckers/ { return 404; }
+location ^~ /case/carlstedt/ { return 404; }
+location ^~ /case/stockmusic/ { return 404; }
+location ^~ /case/svenskbyggtjanst/ { return 404; }
+location ^~ /case/omvarldsbevakning/ { return 404; }
+location ^~ /case/projektbevakning/ { return 404; }
+
+# permanent redirects
+if ($request_uri ~ "om-oss") {
+  rewrite /om\-oss/(.*) /aboutus/$1 permanent; break;
+}
+if ($request_uri ~ medarbetare) {
+  rewrite /medarbetare/(.*) /coworkers/$1 permanent; break;
+}
+if ($request_uri ~ karriar) {
+  rewrite /karriar/(.*) /career/$1 permanent; break;
+}
+
+# temporary redirects
+if ($request_uri ~ "vara-tjanster") {
+  rewrite /vara\-tjanster/(.*) /$1 redirect; break;
+}
+if ($request_uri ~ fjarrhjalp) {
+  rewrite /fjarrhjalp/(.*) /remoteassistance/$1 redirect; break;
+}
+if ($request_uri ~ operations) {
+  rewrite /operations/(.*) /remoteassistance/$1 redirect; break;
+}
