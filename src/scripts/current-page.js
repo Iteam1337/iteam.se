fe5c70ee83a35e801393d2f9ec9839e9ca67a6d9
Dennis Pettersson <mail@dennispettersson.se>
2016-01-04 13:48:02 +0100

    Styling for mobile-menu

--- src/scripts/current-page.js
+++ src/scripts/current-page.js
@@ -1,15 +1,23 @@
 (function () {
   'use strict';
-
-  var path = window.location.pathname.split('/')[1];
+  var paths = window.location.pathname.split('/');
+  var firstPath = '/' + paths[1];
+  var secondPath = '/' + paths[1] + '/' + paths[2];
   var navbar = document.querySelectorAll('#navbar ul li');
 
   for (var i = 0; i < navbar.length; i++) {
-    var child = navbar[i].children[0];
+    var children = navbar[i].children;
+    var child = children[0];
+    var childHref = child.getAttribute('href');
+    var subChild = children[1] ? children[1].children[0] : null;
+    var subChildHref = subChild ? subChild.getAttribute('href') : null;
 
-    if (child.getAttribute('href').substr(1) === path) {
+    if (childHref === firstPath) {
       child.classList.add('active');
     }
-  }
 
+    if (secondPath && subChildHref === secondPath) {
+      subChild.classList.add('active');
+    }
+  }
 })();
