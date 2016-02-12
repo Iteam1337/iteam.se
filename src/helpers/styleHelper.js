1e60e1adcf0030111a555da4088779a6b3f6789f
Dennis Pettersson <mail@dennispettersson.se>
2016-02-12 22:57:06 +0100

    Major structure changes
    Removed all usage of mr-white-mixins -- since most where only for
    browser-prefixing, and that is done by autoprefixer
    
    Renamed fullview-helper to hero, since it's more descriptive
    
    Added a common helper for printing classnames and styles

---
+++ src/helpers/styleHelper.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const hexFromString = require('./hexFromString')
+
+function styleHelper(context) {
+  if (typeof context !== 'object' || !Object.keys(context).length) {
+    return
+  }
+
+  let style = ''
+  let hasBackground = false
+
+  if (context.hasOwnProperty('height')) {
+    style += `height: ${context.height};`
+  }
+  if (context.hasOwnProperty('min-height')) {
+    style += `min-height: ${context['min-height']};`
+  }
+  if (context.hasOwnProperty('background-size')) {
+    style += `background-size: ${context['background-size']};`
+  }
+  if (context.hasOwnProperty('padding-top')) {
+    style += `padding-top: ${context['padding-top']};`
+  }
+  if (context.hasOwnProperty('background-color')) {
+    style += `background-color: ${hexFromString(context['background-color'])};`
+  }
+  if (context.hasOwnProperty('background-image')) {
+    hasBackground = true
+    style += `background-image: url('${context['background-image']}');`
+  }
+  if (context.hasOwnProperty('background-image-url')) {
+    hasBackground = true
+    style += `background-image: url('${context['background-image-url']}');`
+  }
+  if (context.hasOwnProperty('background-position')) {
+    style += `background-position: ${context['background-position']};`
+  } else if (hasBackground) {
+    style += `background-position: center center;`
+  }
+
+  return style
+}
+
+module.exports = styleHelper
